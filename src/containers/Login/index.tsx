7261002a6ee13455425b82e244d3f0765287096e
麦锦富 <https://gitee.com/mjf_cola/config.git>
2023-08-30 23:37:35 +0800

    增加登录页的请求逻辑

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,8 +1,37 @@
+import { useState } from 'react';
 import './style.scss';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 
 const Login = () => {
+
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [date, SetDate] = useState(null);
+    const [loaded, setLoaded] = useState(false);
+    const [error, setError] = useState("");
+
+    function handleSubmitBtnClick() {
+        axios.get('/a.json').then((response) => {
+            setLoaded(true);
+            SetDate(response.data);
+        }).catch((error) => {
+            setLoaded(true);
+            setError(error.message);
+        })
+    }
+
+    if (loaded) {
+        setLoaded(false);
+        if (date) {
+            alert('请求成功')
+        } else {
+            alert(error);
+        }
+    }
+
     const navigate = useNavigate();
     const handleItemRightClick = () => {
         navigate('/sign');
@@ -19,14 +48,25 @@ const Login = () => {
             <div className="form">
                 <div className="form-item">
                     <div className="form-item-title">手机号</div>
-                    <input className="form-item-content" placeholder="请输入手机号码" />
+                    <input
+                        className="form-item-content"
+                        placeholder="请输入手机号码"
+                        value={phoneNumber}
+                        onChange={e => setPhoneNumber(e.target.value)}
+                    />
                 </div>
                 <div className="form-item">
                     <div className="form-item-title">密码</div>
-                    <input className="form-item-content" placeholder="请输入密码" type="password" />
+                    <input
+                        className="form-item-content"
+                        placeholder="请输入密码"
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
                 </div>
             </div>
-            <div className="submit">
+            <div className="submit" onClick={handleSubmitBtnClick}>
                 登录
             </div>
             <p className='notice'>
