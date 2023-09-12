0a31afbfdaf4066acf810385b69b77156139effe
colahaha666 <1600805408@qq.com>
2023-09-12 15:59:21 +0800

    增加category页部分逻辑代码

--- src/containers/Category/index.tsx
+++ src/containers/Category/index.tsx
@@ -1,6 +1,66 @@
+import { useEffect, useState } from 'react';
+import useRequest from '../../hook/useRequest';
 import './style.scss';
+import { CategoryAndTagResponseType, ProductResponseType, ProductType } from './types';
+import { message } from '../../utils/message';
 
 const Category = () => {
+    const [products, setProducts] = useState<Array<ProductType>>([])
+    const [categories, setCategories] = useState<Array<{ id: string; name: string }>>([])
+    const [tags, setTags] = useState<string[]>([]);
+    const [keyword, setKeyword] = useState('');
+
+    const [productRequestData, setProductRequestData] = useState({
+        url: '/category-search-list.json',
+        method: 'POST',
+        data: {
+            tag: '',
+            keyword: '',
+            category: ''
+        }
+    })
+    const { request } = useRequest<CategoryAndTagResponseType>({ manual: true });
+    const { request: productRequest } = useRequest<ProductResponseType>({ manual: true });
+    useEffect(() => {
+        request({
+            url: '/category-list.json',
+            method: 'GET',
+        }).then(data => {
+            if (data?.success) {
+                const result = data.data;
+                setCategories(result.category)
+                setTags(result.tag)
+            }
+        }).catch(e => {
+            message(e?.message)
+        })
+    }, [request])
+
+    useEffect(() => {
+        productRequest({
+            url: '/category-search-list.json',
+            method: 'POST',
+            data: {
+                tag: '',
+                keyword,
+                category: ''
+            }
+        }).then(data => {
+            if (data?.success) {
+                const result = data.data;
+                setProducts(result)
+            }
+        }).catch(e => {
+            message(e?.message)
+        })
+    }, [productRequest, keyword])
+
+    function handleKeyDown(key: string, target: any) {
+        if (key === 'Enter') {
+            setKeyword(target.value)
+        }
+    }
+
     return (
         <div className='page category-page'>
             <div className="title">
@@ -13,124 +73,53 @@ const Category = () => {
                         type="text"
                         className="search-input"
                         placeholder='请输入商品名称'
+                        onKeyDown={e => handleKeyDown(e.key, e.target)}
                     />
                 </div>
             </div>
             <div className="category">
-                <div className="category-item category-item-active">新鲜水果</div>
-                <div className="category-item">新鲜水果</div>
-                <div className="category-item">新鲜水果</div>
-                <div className="category-item">新鲜水果</div>
-                <div className="category-item">新鲜水果</div>
-                <div className="category-item">新鲜水果</div>
-                <div className="category-item">新鲜水果</div>
-                <div className="category-item">新鲜水果</div>
-                <div className="category-item">新鲜水果</div>
-                <div className="category-item">新鲜水果</div>
-                <div className="category-item">新鲜水果</div>
+                <div className="category-item category-item-active">全部商品</div>
+                {
+                    (categories || []).map(item => {
+                        return (
+                            <div className="category-item" key={item.id}>{item.name}</div>
+                        )
+                    })
+                }
             </div>
             <div className="tag">
                 <div className="tag-item tag-item-active">全部</div>
-                <div className="tag-item">全部</div>
-                <div className="tag-item">全部</div>
-                <div className="tag-item">全部</div>
-                <div className="tag-item">全部</div>
-                <div className="tag-item">全部</div>
-                <div className="tag-item">全部</div>
-                <div className="tag-item">全部</div>
+                {
+                    tags.map((item, index) => {
+                        return (
+                            <div className="tag-item" key={item + index}>{item}</div>
+                        )
+                    })
+                }
             </div>
             <div className="product">
-                <div className="product-title">精选商品（50）</div>
-                <div className="product-item">
-                    <img
-                        className="product-item-image"
-                        src='http://statics.dell-lee.com/shopping/category-list-1.png'
-                        alt=''
-                    />
-                    <div className="product-item-content">
-                        <div className="product-item-title">华都食品 鸡翅中 1000g/ ...</div>
-                        <div className="product-item-sales">月售156</div>
-                        <div className="product-item-price">
-                            <span className="product-item-price-yen">&yen;</span>59.9
-                        </div>
-                        <div className="product-item-button">购买</div>
-                    </div>
-                </div>
-                <div className="product-item">
-                    <img
-                        className="product-item-image"
-                        src='http://statics.dell-lee.com/shopping/category-list-1.png'
-                        alt=''
-                    />
-                    <div className="product-item-content">
-                        <div className="product-item-title">华都食品 鸡翅中 1000g/ ...</div>
-                        <div className="product-item-sales">月售156</div>
-                        <div className="product-item-price">
-                            <span className="product-item-price-yen">&yen;</span>59.9
-                        </div>
-                        <div className="product-item-button">购买</div>
-                    </div>
-                </div>
-                <div className="product-item">
-                    <img
-                        className="product-item-image"
-                        src='http://statics.dell-lee.com/shopping/category-list-1.png'
-                        alt=''
-                    />
-                    <div className="product-item-content">
-                        <div className="product-item-title">华都食品 鸡翅中 1000g/ ...</div>
-                        <div className="product-item-sales">月售156</div>
-                        <div className="product-item-price">
-                            <span className="product-item-price-yen">&yen;</span>59.9
-                        </div>
-                        <div className="product-item-button">购买</div>
-                    </div>
-                </div>
-                <div className="product-item">
-                    <img
-                        className="product-item-image"
-                        src='http://statics.dell-lee.com/shopping/category-list-1.png'
-                        alt=''
-                    />
-                    <div className="product-item-content">
-                        <div className="product-item-title">华都食品 鸡翅中 1000g/ ...</div>
-                        <div className="product-item-sales">月售156</div>
-                        <div className="product-item-price">
-                            <span className="product-item-price-yen">&yen;</span>59.9
-                        </div>
-                        <div className="product-item-button">购买</div>
-                    </div>
-                </div>
-                <div className="product-item">
-                    <img
-                        className="product-item-image"
-                        src='http://statics.dell-lee.com/shopping/category-list-1.png'
-                        alt=''
-                    />
-                    <div className="product-item-content">
-                        <div className="product-item-title">华都食品 鸡翅中 1000g/ ...</div>
-                        <div className="product-item-sales">月售156</div>
-                        <div className="product-item-price">
-                            <span className="product-item-price-yen">&yen;</span>59.9
-                        </div>
-                        <div className="product-item-button">购买</div>
-                    </div>
-                </div>
-                <div className="product-item">
-                    <img
-                        className="product-item-image"
-                        src='http://statics.dell-lee.com/shopping/category-list-1.png'
-                        alt=''
-                    />
-                    <div className="product-item-content">
-                        <div className="product-item-title">华都食品 鸡翅中 1000g/ ...</div>
-                        <div className="product-item-sales">月售156</div>
-                        <div className="product-item-price">
-                            <span className="product-item-price-yen">&yen;</span>59.9
-                        </div>
-                        <div className="product-item-button">购买</div>
-                    </div>
-                </div>
+                <div className="product-title">精选商品（{products.length}）</div>
+                {
+                    products.map(item => {
+                        return (
+                            <div className="product-item" key={item.id}>
+                                <img
+                                    className="product-item-img"
+                                    src={item.imgUrl}
+                                    alt={item.title}
+                                />
+                                <div className="product-item-content">
+                                    <div className="product-item-title">{item.title}</div>
+                                    <div className="product-item-sales">月售{item.sales}</div>
+                                    <div className="product-item-price">
+                                        <span className="product-item-price-yen">&yen;</span>{item.price}
+                                    </div>
+                                    <div className="product-item-button">购买</div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
 
             <div className="docker">
--- src/containers/Category/types.ts
+++ src/containers/Category/types.ts
@@ -1,9 +1,25 @@
-export type ResponseType = {
+export type CategoryAndTagResponseType = {
+    success: boolean,
+    data: {
+        category: Array<{
+            id: string,
+            name: string
+        }>,
+        tag: Array<string>
+    },
 
-    sussess: boolean,
-    data: Array<{
-        id: string,
-        keyword: string
-    }>,
+}
+
+export type ProductType = {
+    id: string,
+    imgUrl: string,
+    title: string,
+    price: number,
+    sales: number,
+}
+
+export type ProductResponseType = {
+    success: boolean,
+    data: Array<ProductType>
 
 }
