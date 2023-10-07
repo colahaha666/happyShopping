c20dcf0e130ee8c9eb60107f6524bef1e271be23
麦锦富 <https://gitee.com/mjf_cola/config.git>
2023-10-07 14:19:02 +0800

    完成my页面

--- src/containers/My/index.tsx
+++ src/containers/My/index.tsx
@@ -1,14 +1,39 @@
 import Docker from '../../components/Docker';
 import './style.scss'
 
-const My = () => {
+const icons = [{
+    icon: '&#xe601;',
+    text: '全部订单',
+}, {
+    icon: '&#xe629;',
+    text: '待付款',
+}, {
+    icon: '&#xe640;',
+    text: '待发货',
+}, {
+    icon: '&#xe634;',
+    text: '待收货',
+}, {
+    icon: '&#xe6c3;',
+    text: '退款/售后',
+}, {
+    icon: '&#xe649;',
+    text: '客服',
+}, {
+    icon: '&#xe78e;',
+    text: '设置',
+}, {
+    icon: '&#xe61c;',
+    text: '地址',
+},]
 
+const My = () => {
     return (
         <div className='page my-page'>
             <div className="my">
                 <div className="title">我的</div>
                 <div className="my-info">
-                    <img className="my-info-img" src='' alt='' />
+                    <img className="my-info-img" src={require('../../images/头像_@2x.png')} alt='' />
                     <div className="my-info-name">王翠花</div>
                     <div className="my-info-icon">VIP5</div>
                     <div className="my-info-button">会员中心</div>
@@ -24,38 +49,14 @@ const My = () => {
                     </div>
                 </div>
                 <div className="my-functions">
-                    <div className="my-functions-item">
-                        <div className="my-functions-item-icon iconfont">&#xe61c;</div>
-                        <div className="my-functions-item-text">全部订单</div>
-                    </div>
-                    <div className="my-functions-item">
-                        <div className="my-functions-item-icon iconfont">&#xe61c;</div>
-                        <div className="my-functions-item-text">全部订单</div>
-                    </div>
-                    <div className="my-functions-item">
-                        <div className="my-functions-item-icon iconfont">&#xe61c;</div>
-                        <div className="my-functions-item-text">全部订单</div>
-                    </div>
-                    <div className="my-functions-item">
-                        <div className="my-functions-item-icon iconfont">&#xe61c;</div>
-                        <div className="my-functions-item-text">全部订单</div>
-                    </div>
-                    <div className="my-functions-item">
-                        <div className="my-functions-item-icon iconfont">&#xe61c;</div>
-                        <div className="my-functions-item-text">全部订单</div>
-                    </div>
-                    <div className="my-functions-item">
-                        <div className="my-functions-item-icon iconfont">&#xe61c;</div>
-                        <div className="my-functions-item-text">全部订单</div>
-                    </div>
-                    <div className="my-functions-item">
-                        <div className="my-functions-item-icon iconfont">&#xe61c;</div>
-                        <div className="my-functions-item-text">全部订单</div>
-                    </div>
-                    <div className="my-functions-item">
-                        <div className="my-functions-item-icon iconfont">&#xe61c;</div>
-                        <div className="my-functions-item-text">全部订单</div>
-                    </div>
+                    {icons.map(item => {
+                        return (
+                            <div className="my-functions-item">
+                                <div className="my-functions-item-icon iconfont" dangerouslySetInnerHTML={{ __html: item.icon }}></div>
+                                <div className="my-functions-item-text">{item.text}</div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
             <Docker activeName='我的'></Docker>
